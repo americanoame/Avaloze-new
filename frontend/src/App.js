409a08efd8191bbacb86901334b2c8bb6cc0a99a
Americano.Dev <andrenations@gmail.com>
2023-06-05 20:51:32 -0400

    added header

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import { Container } from 'react-bootstrap';
+// import {  Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <Container className="d-flex justify-content-center align-items-center first-nav">
+        <nav>Free delivery over $100 anywhere in the US/30 days return free</nav>
+      </Container>
+      <header>
+        <a href="/">avaloze-new</a>
+      </header>
+      <main>list products</main>
+    </div>
+  );
+}
+
+export default App;
